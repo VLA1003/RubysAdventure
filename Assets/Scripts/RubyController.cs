c7fbd5c00536152c117645616026b85f242c1eee
paulamgonzalez <11alvaro11alonso@gmail.com>
2023-03-09 09:12:21 +0000

    Segundo commit
    // Sistema de movimiento

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    void Start()
+    {
+
+    }
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
